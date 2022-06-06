9ab8ac6efe89f1c389bd60e0e3149b83834ff031
Ondicho <albert.ondicho@moringaschool.com>
2022-06-06 09:47:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(Array) {
+  for (let item of Array) {
+    if(item.result === "W"){
+        return item.year
+    }
+    
+  }
+
+}
+
+Array.find(superbowlWin)
